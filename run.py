cf2fdcaa9a22f67e3647374c691957f3054ba14d
Kacper <kacper@kacperserewis.net>
2021-09-24 22:21:51 +0200

    add docker

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app.__main__ import App
-from app.models.dummy_lcd import DummyLcd
 from app.models.thermalzone_temp import ThermalZoneTemp
 from app.models.psutil_memory import PsutilMemory
 from app.models.uptime_cpu import UptimeCPU
+from app.models.i2c_lcd import I2CLcd
+from time import sleep
 
-app = App(DummyLcd(), [ThermalZoneTemp(), PsutilMemory(), UptimeCPU()])
+app = App(I2CLcd(), [ThermalZoneTemp(), PsutilMemory(), UptimeCPU()])
 while True:
     app.loop()
+    sleep(0.5)
